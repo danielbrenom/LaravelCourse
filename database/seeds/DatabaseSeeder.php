4ba2e3e2733c4d3c10bdfb2d89fc4cdd1d7d92b9
Daniel Guiomarino <daniel.guiomarino@tradeupgroup.com>
2020-05-30 13:02:30 -0300

    Buyer controller

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,32 +1,32 @@
 <?php
 
-use App\User;
-use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use App\Domain\Models\Tables\Category;
 use App\Domain\Models\Tables\Product;
 use App\Domain\Models\Tables\Transaction;
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
-    private const USER_QUANTITY = 20;
+    private const USER_QUANTITY = 100;
     private const CATEGORY_QUANTITY = 20;
     private const PRODUCT_QUANTITY = 100;
-    private const TRANSACTION_QUANTITY = 200;
+    private const TRANSACTION_QUANTITY = 50;
 
     /**
      * Seed the application's database.
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
         DB::statement('SET FOREIGN_KEY_CHECKS = 0');
         $this->truncateTables();
         $this->seedTables();
     }
 
-    public function truncateTables()
+    public function truncateTables(): void
     {
         DB::table('users')->truncate();
         DB::table('categories')->truncate();
@@ -35,7 +35,7 @@ class DatabaseSeeder extends Seeder
         DB::table('category_product')->truncate();
     }
 
-    private function seedTables()
+    private function seedTables(): void
     {
         factory(User::class, self::USER_QUANTITY)->create();
         factory(Category::class, self::CATEGORY_QUANTITY)->create();
--- modules/Buyer/Http/Controllers/BuyerController.php
+++ modules/Buyer/Http/Controllers/BuyerController.php
@@ -2,84 +2,22 @@
 
 namespace Modules\Buyer\Http\Controllers;
 
+use App\Domain\Models\Tables\Buyer;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 
 class BuyerController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function index(): JsonResponse
     {
-        //
+        $buyers = Buyer::has('transactions')->get();
+        return response()->json(['data' => $buyers], Response::HTTP_OK);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function show($id): JsonResponse
     {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $buyer = Buyer::has('transactions')->findOrFail($id);
+        return response()->json(['data' => $buyer], Response::HTTP_OK);
     }
 }
