185576b21236121aa96971dc9622403d8b53e478
Daniel Guiomarino <daniel.guiomarino@tradeupgroup.com>
2020-05-30 13:06:47 -0300

    Fix

--- modules/Seller/Http/Controllers/SellerController.php
+++ modules/Seller/Http/Controllers/SellerController.php
@@ -2,29 +2,20 @@
 
 namespace Modules\Seller\Http\Controllers;
 
+use App\Domain\Models\Tables\Seller;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 
 class SellerController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return JsonResponse
-     */
+
     public function index(): JsonResponse
     {
         $seller = Seller::has('products')->get();
         return response()->json(['data' => $seller], Response::HTTP_OK);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param int $id
-     * @return JsonResponse
-     */
     public function show($id): JsonResponse
     {
         $seller = Seller::has('products')->findOrFail($id);
